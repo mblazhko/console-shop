a2fe2e434f4ba4599f235dd0b668052a00b2c156
neostyle88 <makssimm1@gmail.com>
2023-09-21 15:40:42 +0200

    add DBProduct model to product

---
+++ product/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Numeric
+from database import Base
+
+
+class DBProduct(Base):
+    __tablename__ = "product"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+    name = Column(String(255), nullable=False)
+    type = Column(String(511), nullable=False)
+    daily_fee = Column(Numeric(precision=10, scale=2), nullable=True)
