61b9b4b8f37a802a7a310bc585e8610e5eea9bff
neostyle88 <makssimm1@gmail.com>
2023-09-21 19:51:26 +0200

    define specific metadata for product

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from product import *
-
-from database import Base
+from product.models import DBProduct
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = [DBProduct.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
