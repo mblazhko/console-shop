b010ba1a60a01e02e956af8c501d120465a9272e
neostyle88 <makssimm1@gmail.com>
2023-09-22 19:12:10 +0200

    implement borrowing model

---
+++ borrowing/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, DateTime, func, Boolean
+from database import Base
+
+
+class DBBorrowing(Base):
+    __tablename__ = "borrowing"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+    product_id = Column(Integer, nullable=False)
+    payment_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, nullable=False)
+    borrow_date = Column(DateTime, server_default=func.now(), nullable=False)
+    expected_return_date = Column(DateTime, nullable=False)
+    actual_return_date = Column(DateTime, nullable=True)
+    is_active = Column(Boolean, nullable=True, default=True)
