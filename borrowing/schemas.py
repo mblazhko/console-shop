c17830a4634f89cd154660d7782b1980a49971ab
neostyle88 <makssimm1@gmail.com>
2023-09-23 10:46:17 +0200

    add validators for fields with datetime type

--- borrowing/schemas.py
+++ borrowing/schemas.py
@@ -1,6 +1,6 @@
 import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class BorrowingBase(BaseModel):
@@ -12,4 +12,34 @@ class BorrowingBase(BaseModel):
     actual_return_date: datetime.datetime
     is_active: bool
 
+    @field_validator("borrow_date", "expected_return_date")
+    @classmethod
+    def borrow_date_lte_expected_return_date(cls, values):
+        if values["borrow_date"] > values["expected_return_date"]:
+            raise ValueError(
+                "Borrow date must be less than or equal to expected return date"
+            )
 
+    @field_validator("borrow_date", "actual_return_date")
+    @classmethod
+    def borrow_date_lte_actual_return_date(cls, values):
+        if values["borrow_date"] > values["actual_return_date"]:
+            raise ValueError(
+                "Borrow date must be less than or equal to actual return date"
+            )
+
+    @field_validator("expected_return_date", "borrow_date")
+    @classmethod
+    def expected_return_date_gte_borrow_date(cls, values):
+        if values["expected_return_date"] < values["borrow_date"]:
+            raise ValueError(
+                "Expected return date must be greater than or equal to borrow date"
+            )
+
+    @field_validator("actual_return_date", "borrow_date")
+    @classmethod
+    def actual_return_date_gte_borrow_date(cls, values):
+        if values["actual_return_date"] < values["borrow_date"]:
+            raise ValueError(
+                "Actual return date must be greater than or equal to borrow date"
+            )
