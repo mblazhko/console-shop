4cc2f04e99b89df0ea3ff493716e18ac821cea42
neostyle88 <makssimm1@gmail.com>
2023-09-23 12:33:51 +0200

    add borrowing router

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from product import router as product_router
+from borrowing import router as borrowing_router
 
 app = FastAPI()
 app.include_router(product_router.router)
-
+app.include_router(borrowing_router.router)
 
 @app.get("/")
 async def root():
