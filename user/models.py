3dfc230eb6f76e5182f2229eb0bdecd853e7c870
neostyle88 <makssimm1@gmail.com>
2023-09-24 18:48:26 +0200

    add User & OAuthAccount models

---
+++ user/models.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseOAuthAccountTableUUID, \
+    SQLAlchemyBaseUserTableUUID
+from sqlalchemy.orm import Mapped, relationship
+
+from database import Base
+
+
+class OAuthAccount(SQLAlchemyBaseOAuthAccountTableUUID, Base):
+    pass
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    oauth_accounts: Mapped[List[OAuthAccount]] = relationship(
+        "OAuthAccount", lazy="joined"
+    )
+
