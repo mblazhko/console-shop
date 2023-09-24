0dedbce8b377044d7fe2cbbc5a2b2dcf309f9844
neostyle88 <makssimm1@gmail.com>
2023-09-24 18:56:44 +0200

    add get_user_db dependence

--- dependencies.py
+++ dependencies.py
@@ -1,6 +1,9 @@
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import SessionLocal
+from user.models import OAuthAccount, User
 
 
 async def get_db() -> AsyncSession:
@@ -10,3 +13,7 @@ async def get_db() -> AsyncSession:
         yield db
     finally:
         await db.close()
+
+
+async def get_user_db(session: AsyncSession = Depends(SessionLocal)):
+    yield SQLAlchemyUserDatabase(session, User, OAuthAccount)
