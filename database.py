07575cf7bc713496ffe51958c27dd7397999b408
neostyle88 <makssimm1@gmail.com>
2023-09-24 18:50:17 +0200

    change sessionmaker to async_sessionmaker

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 from settings import settings
 
@@ -9,7 +8,7 @@ SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(
+SessionLocal = async_sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine,
