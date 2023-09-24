ad23f4b44f18d6c8617c9c9de2e47374b24cc901
neostyle88 <makssimm1@gmail.com>
2023-09-24 10:41:52 +0200

    add validator for inventory & daily_fee

--- product/schemas.py
+++ product/schemas.py
@@ -1,6 +1,6 @@
 import decimal
 
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, condecimal, field_validator
 from enum import Enum
 
 
@@ -12,8 +12,15 @@ class ProductType(str, Enum):
 class ProductBase(BaseModel):
     name: str
     type: ProductType
-    daily_fee: decimal.Decimal
-    inventory: PositiveInt
+    daily_fee: condecimal(ge=decimal.Decimal('0.00'), max_digits=10, decimal_places=2)
+    inventory: int
+
+    @field_validator("inventory")
+    @classmethod
+    def validate_inventory(cls, values):
+        if values is not None and values < 0:
+            raise ValueError("Inventory must be a non-negative integer")
+        return values
 
 
 class ProductCreate(ProductBase):
